13ade1f357e0aae005613ba0a1a0b1e3dd3f1e63
gbabudoh <gbabudoh@gmail.com>
2026-04-16 04:16:21 +0100

    feat: enable hero image on mobile devices

--- app/page.tsx
+++ app/page.tsx
@@ -183,7 +183,7 @@ export default function HomePage() {
               initial={{ opacity: 0, x: 50 }}
               animate={{ opacity: 1, x: 0 }}
               transition={{ duration: 0.8, delay: 0.2, type: "spring" }}
-              className="relative hidden lg:block"
+              className="relative mt-12 lg:mt-0"
             >
               <motion.div
                 animate={{
